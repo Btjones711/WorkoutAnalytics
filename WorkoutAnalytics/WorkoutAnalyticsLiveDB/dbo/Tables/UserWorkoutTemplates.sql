7f000e2a57d2726c15b217d6240e634ddf75a13c
Btjones711 <Btjones711@gmail.com>
2019-05-12 16:55:46 -0400

    Update databases for template support

---
+++ WorkoutAnalytics/WorkoutAnalyticsLiveDB/dbo/Tables/UserWorkoutTemplates.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserWorkoutTemplates]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [TemplateID] INT NOT NULL, 
+    [WorkoutID] INT NOT NULL, 
+    CONSTRAINT [FK_UserWorkoutTemplates_ToTemplate] FOREIGN KEY (TemplateID) REFERENCES UserTemplates(TemplateID), 
+    CONSTRAINT [FK_UserWorkoutTemplates_ToWorkOuts] FOREIGN KEY (WorkoutID) REFERENCES Workouts(WorkoutID)
+)
---
+++ WorkoutAnalytics/WorkoutAnalyticsTestDB/dbo/Tables/UserTemplates.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[UserTemplates] (
+    [TemplateID]   INT           IDENTITY (1, 1) NOT NULL,
+    [UserID]       INT           NOT NULL,
+    [TemplateDesc] VARCHAR (MAX) NOT NULL,
+    [DayOfTheWeek] INT           NULL,
+    PRIMARY KEY CLUSTERED ([TemplateID] ASC),
+    CONSTRAINT [FK_UserTemplates_ToTable] FOREIGN KEY ([UserID]) REFERENCES [dbo].[User] ([UserId])
+);
+
